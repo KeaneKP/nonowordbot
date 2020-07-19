a7d8027d9354b903f68c9ead1515e458ea315f86
KeaneKP <57782681+KeaneKP@users.noreply.github.com>
2020-07-19 13:12:47 -0400

    COmbined with debtime boi

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const badWords = require("./badWords.json");
+let hasBedTimed = false;
+let utcHour = 0;
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -13,6 +15,27 @@ client.on('message', message => {
       return;
     }
   })
+  let date = new Date();
+  let hour = date.getHours();
+  if (message.member.id=="732613702267961374" && hour>=0 && hour<=10) {
+    message.react("🛏️");
+  }
 });
-
+setInterval(() => {
+  let date = new Date();
+  let hour = date.getHours();
+  if (hour===utcHour && !hasBedTimed) {
+    let guildsArray = client.guilds.array()
+    guildsArray.forEach(guild => {
+      //console.log(guild.channels);
+      let channelArray = Array.from(guild.channels);
+      let general = channelArray.find(channel => {return channel[1].name == "general"});
+      general[1].send("<@732613702267961374>https://cdn.discordapp.com/attachments/688083956385710093/688091189710159951/SHAWNBED.png")
+    });
+    hasBedTimed = true;
+  } else if (hour != utcHour) {
+    hasBedTimed = false;
+  }
+}, 15000);
+client.login(process.env.token);
 client.login(process.env.token);
